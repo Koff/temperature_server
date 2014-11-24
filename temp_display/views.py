83d1b93a54ed2bc91bdeb85e6842b6952cf4dec9
Koff <fsanmartinjorquera@uh.edu>
2014-11-23 21:33:01 -0800

    Main Page, 3 graphs already shown: Hourly, Daily and Weekly

--- temp_display/views.py
+++ temp_display/views.py
@@ -1,27 +1,20 @@
-import datetime
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from django.views import generic
 from models import Temps
 
 # Create your views here.
 
 
-class Last5Reads(generic.ListView):
+class Main(generic.ListView):
     template_name = 'temp_display/index.html'
-    context_object_name = 'latest_temps_list'
+    context_object_name = 'last_hour_temperature'
 
     def get_queryset(self):
-        temporal_result = Temps.objects.order_by('-time_stamp')[:5]
+        temporal_result = Temps.objects.order_by('-time_stamp')[:13]
         return temporal_result[::-1]
 
-
-class Last24Hours(generic.ListView):
-    template_name = 'temp_display/daily_detail.html'
-    context_object_name = 'last_24h_temps_list'
-
-    def get_queryset(self):
-        temporal_result = Temps.objects.filter(time_stamp__gte=(datetime.datetime.today() - datetime.timedelta(hours=24)))
-        print(type(temporal_result))
-        return temporal_result
+    def get_context_data(self, **kwargs):
+        context = super(Main, self).get_context_data(**kwargs)
+        context['last_24_hours'] = Temps.objects.raw("SELECT CONCAT(date(time_stamp), ' ', HOUR(time_stamp), ':00:00') AS time_stamp, AVG(temperature) AS temperature FROM temps.temp_display_temps WHERE time_stamp >= NOW() - INTERVAL 24 HOUR GROUP BY date(time_stamp), HOUR(time_stamp)")
+        context['last_1_week'] = Temps.objects.raw("SELECT CONCAT(DAY(time_stamp),'-',DAY(time_stamp)) AS time_stamp, AVG(temperature) AS temperature FROM temps.temp_display_temps WHERE time_stamp >= NOW() - INTERVAL 7 DAY GROUP BY DAY(time_stamp)")
+        context['last_1_month'] = Temps.objects.raw("SELECT CONCAT(DAY(time_stamp),'-',MONTH(time_stamp)) AS time_stamp, AVG(temperature) AS temperature FROM temps.temp_display_temps WHERE time_stamp >= NOW() - INTERVAL 30 DAY GROUP BY DAY(time_stamp)")
+        return context
--- temp_server/urls.py
+++ temp_server/urls.py
@@ -5,6 +5,5 @@ from temp_display import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^temps/$', views.Last5Reads.as_view(), name='index'),
-    url(r'^temps/24hours', views.Last24Hours.as_view(), name='detail_last_24_hours'),
+    url(r'^temps/$', views.Main.as_view(), name='index'),
 )
